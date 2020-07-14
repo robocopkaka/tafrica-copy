31711ef59cb112030bad2c672272e50fea974f33
robocopkaka <robocopkaka@gmail.com>
2020-07-14 17:07:47 +0100

    add page for creating events   - add events model   - add events controller   - add view for creating new event   - add error messages partial   - add events' routes

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_13_160753) do
+ActiveRecord::Schema.define(version: 2020_07_14_131656) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+# Could not dump table "events" because of following StandardError
+#   Unknown type 'types' for column 'event_type'
 
   create_table "users", force: :cascade do |t|
     t.string "name", default: "", null: false
